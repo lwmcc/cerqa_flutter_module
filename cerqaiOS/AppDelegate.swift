156160ef1784ff7a1d1a12a5c916b2726e3f0cd9
Larry McCarty <lwmccarty@gmail.com>
2025-07-17 16:15:46 -0700

    Fix type

---
+++ cerqaiOS/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+// Created by LARRY MCCARTY on 7/15/25.
+//
+
+import Foundation
+import UIKit
+import Amplify
+import AmplifyPlugins
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        do {
+            try Amplify.add(plugin: AWSCognitoAuthPlugin())
+            try Amplify.add(plugin: AWSAPIPlugin())
+            try Amplify.configure()
+        } catch {
+            // Handle error
+        }
+        return true
+    }
+}
---
+++ cerqaiOS/cerqaiOS-Bridging-Header.h
@@ -0,0 +1,1 @@
+#import "cerqaiOS-Swift.h"
