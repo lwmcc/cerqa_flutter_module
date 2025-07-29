99656569fc550e2bd8a5f53946685fcef8c5476b
Larry McCarty <lwmccarty@gmail.com>
2025-07-28 23:39:17 -0700

    Add function calls. Add data models that will be used for chat

--- app/src/main/java/com/mccartycarclub/repository/ChatContacts.kt
+++ app/src/main/java/com/mccartycarclub/repository/ChatContacts.kt
@@ -2,4 +2,8 @@ package com.mccartycarclub.repository
 
 interface ChatContacts {
     fun fetchChats()
+    fun fetchGroupsChats()
+    fun createMessage()
+    fun deleteMessage()
+    fun startChat()
 }
